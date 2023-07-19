3aef31455c4cbd3dac675faac3b51688fb5431c7
Asma_syam <Asma.I.Syam@gmail.com>
2023-07-19 14:06:48 +0300

    Update DetailsActivity

--- app/src/main/java/com/example/space_ucas_ti_asmasyam/DetailsActivity.java
+++ app/src/main/java/com/example/space_ucas_ti_asmasyam/DetailsActivity.java
@@ -4,16 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.space_ucas_ti_asmasyam.databinding.ActivityDetailsBinding;
+import com.google.type.Color;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 
@@ -21,6 +26,8 @@ public class DetailsActivity extends AppCompatActivity {
 
 
     ActivityDetailsBinding binding ;
+
+    int t1Hour, t1Minute ;
 
     Calendar calendar ;
 
@@ -47,7 +54,14 @@ public class DetailsActivity extends AppCompatActivity {
         binding.linearStartTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openDialog();
+                openDialogStartTime();
+            }
+        });
+
+        binding.linearEndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialogEndTime();
             }
         });
 
@@ -55,16 +69,67 @@ public class DetailsActivity extends AppCompatActivity {
 
     }
 
-    private void openDialog() {
+    private void openDialogStartTime() {
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int hours, int minutes) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                DetailsActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
 
-                binding.textStartTime.setText(String.valueOf(hours)+":"+String.valueOf(minutes));
-            }
-        }, 15, 00, true);
+                        //Initialize hour and minute :
+                        t1Hour = hourOfDay ;
+                        t1Minute = minute ;
+
+                        //set hour and minute in calender :
+                        calendar.set(0 , 0 , 0 , t1Hour , t1Minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+
+                        binding.textStartTime.setText(DateFormat.format("hh:mm aa" , calendar));
+                        Toast.makeText(DetailsActivity.this, DateFormat.format("hh:mm aa" , calendar), Toast.LENGTH_SHORT).show();
+
+                    }
+                }, 12 , 0 , false
+        );
+
+       // timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.));
+
+        timePickerDialog.updateTime(t1Hour , t1Minute);
+        timePickerDialog.show();
     }
+
+    private void openDialogEndTime() {
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                DetailsActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+
+                        //Initialize hour and minute :
+                        t1Hour = hourOfDay ;
+                        t1Minute = minute ;
+
+                        //set hour and minute in calender :
+                        calendar.set(0 , 0 , 0 , t1Hour , t1Minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+
+                        binding.textEndTime.setText(DateFormat.format("hh:mm aa" , calendar));
+                        Toast.makeText(DetailsActivity.this, DateFormat.format("hh:mm aa" , calendar), Toast.LENGTH_SHORT).show();
+
+                    }
+                }, 12 , 0 , false
+        );
+
+        // timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.));
+
+        timePickerDialog.updateTime(t1Hour , t1Minute);
+        timePickerDialog.show();
+    }
+
+
 
     public void getDate(){
 
