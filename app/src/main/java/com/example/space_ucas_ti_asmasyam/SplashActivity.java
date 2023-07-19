02869498fc63cd806fcf465f86129b6357dc1e9b
Asma_syam <Asma.I.Syam@gmail.com>
2023-07-19 15:44:37 +0300

    Update SplashActivity

---
+++ app/src/main/java/com/example/space_ucas_ti_asmasyam/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.space_ucas_ti_asmasyam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+        }, 2000);
+    }
+}
